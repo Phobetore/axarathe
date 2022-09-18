ce467c939be55338607f7035dc1854b7d021549f
Phobetore <bourcewayn1413@protonmail.com>
2022-09-18 17:40:13 +0200

    first version of the main page

--- explore/index.php
+++ explore/index.php
@@ -5,7 +5,26 @@ include "../_default/header.php";
 
 <body style="height: 100vh;margin: 0;">
 
-  
+    <section>
+        <!-- image de la map en grand -->
+    </section>
+
+    <style>
+        body {
+            margin: 0;
+            width: 100vw;
+            height: 100vh;
+        }
+
+        section {
+            background-image: url("/content/images/map.png");
+            background-position: left;
+            background-size: contain;
+            background-repeat: no-repeat;
+            width: 70vw;
+            height: 100vh;
+        }
+    </style>
 
 </body>
 
